31f803afc919aa282a409e38cea66b2c5e09e800
PADMALAYA RAWAL <60266976+padmalaya-rawal@users.noreply.github.com>
2020-04-08 21:28:14 +0530

    C5 B a
    chapter 5
    section B
    code a

---
+++ C5_B_a.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+	int e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,sal,e;
+	printf("Enter the total worked hours of employee 1: ");
+	scanf("%d", &e1);
+	
+	printf("Enter the total worked hours of employee 2: ");
+	scanf("%d", &e2);
+	
+	printf("Enter the total worked hours of employee 3: ");
+	scanf("%d", &e3);
+	
+	printf("Enter the total worked hours of employee 4: ");
+	scanf("%d", &e4);
+	
+	printf("Enter the total worked hours of employee 5: ");
+	scanf("%d", &e5);
+	
+	printf("Enter the total worked hours of employee 6: ");
+	scanf("%d", &e6);
+	
+	printf("Enter the total worked hours of employee 7: ");
+	scanf("%d", &e7);
+	
+	printf("Enter the total worked hours of employee 8: ");
+	scanf("%d", &e8);
+	
+	printf("Enter the total worked hours of employee 9: ");
+	scanf("%d", &e9);
+	
+	printf("Enter the total worked hours of employee 10: ");
+	scanf("%d", &e10);	
+	
+	int i=0;
+	while(i<=10)
+	{
+		sal=ei-40;
+		printf("Overtime pay of employee %d: %d",i,sal);
+		i++;
+	}	
+	return 0;
+}	
